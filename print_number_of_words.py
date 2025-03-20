9c73b7dd01ecc55ac3b8c478172c778e8c65f768
aenonod <abegail.nonod13@gmail.com>
2025-03-21 01:55:28 +0800

    add program code to print number of words

--- print_number_of_words.py
+++ print_number_of_words.py
@@ -1,2 +1,6 @@
 # Ask the user to input a complete statement
+statement = input("Input a statement: ")
+
 # Print the number of words in the input
+word_count = statement.split()
+print("Number of words:", word_count.len())
