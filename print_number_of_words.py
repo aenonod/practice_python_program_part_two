ead04bbd34618ffc65cafbc78fcd661dc1376e1d
aenonod <abegail.nonod13@gmail.com>
2025-03-21 01:59:32 +0800

    refactor: combine len() with .split() function for correct result

--- print_number_of_words.py
+++ print_number_of_words.py
@@ -2,5 +2,5 @@
 statement = input("Input a statement: ")
 
 # Print the number of words in the input
-word_count = statement.split()
-print("Number of words:", word_count.len())
+word_count = len(statement.split())
+print("Number of words:", word_count)
