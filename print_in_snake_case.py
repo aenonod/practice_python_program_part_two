5d2dc494931c4109ab7960476b107ea3580dccfc
aenonod <abegail.nonod13@gmail.com>
2025-03-21 02:35:48 +0800

    add program code to convert input to lower case

--- print_in_snake_case.py
+++ print_in_snake_case.py
@@ -1,2 +1,5 @@
 # Ask the user to input their fullname in incorrect casing
+name = input("Input your full name (in incorrect casing): ")
+
 # Print the input in snake case
+lower_case = name.lower()
