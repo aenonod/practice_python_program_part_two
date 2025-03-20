0204c9a44dfab4ed3db1466776c3031dd69fcd02
aenonod <abegail.nonod13@gmail.com>
2025-03-21 00:47:17 +0800

    add program code to print input without spaces

--- print_without_spaces.py
+++ print_without_spaces.py
@@ -1,2 +1,6 @@
 # Ask the user to input their fullname with several space characters at the beginning
+name = input("Input your full name: ")
+
 # Print the input without the spaces in the beginning
+name.strip()
+print(name)
