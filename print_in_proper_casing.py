1c1f249a58acddaa9c80c16282e8af71c0fa392a
aenonod <abegail.nonod13@gmail.com>
2025-03-21 01:42:07 +0800

    add program code to print input in lower case

--- print_in_proper_casing.py
+++ print_in_proper_casing.py
@@ -1,2 +1,5 @@
 # Ask the user to input their fullname in incorrect casing
-# Print the input in proper casing
+name = input("Input your full name (in incorrect casing): ")
+
+# Print the input in proper 
+print(name.title())
