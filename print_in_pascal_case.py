1988f5a212bd1bec84aaaf80aa7ea94d72381cda
aenonod <abegail.nonod13@gmail.com>
2025-03-21 02:18:02 +0800

    add program code to convert input to proper casing

--- print_in_pascal_case.py
+++ print_in_pascal_case.py
@@ -1,2 +1,6 @@
 # Ask the user to input their fullname in incorrect casing
+name = input("Input your full name (in incorrect casing): ")
+
 # Print the input in pascal case
+proper_case = name.title()
+print(proper_case)
