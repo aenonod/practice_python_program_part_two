ca71189a3cf674c16d5f3d89d1ca1bf677fd2433
aenonod <abegail.nonod13@gmail.com>
2025-03-21 01:01:10 +0800

    add program code to print number with zeros to complete six digit

--- add_zeros_to_complete_six_digit.py
+++ add_zeros_to_complete_six_digit.py
@@ -1,3 +1,6 @@
 # Ask the user to input a number (0-1000)
+user_input = input("Input a number (0-1000): ")
+
 # Print the number in 6 digit format
 # Add zeros at the beginning to complete the 6 digit
+print(user_input.zfill(6))
